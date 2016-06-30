9979294456607c90b59543db2ad7c12d21f7c004
aalinemanuel <aalinemanuel@yahoo.com>
2016-06-30 09:35:10 +0300

    Primul commit

---
+++ helloworld/src/main/java/example/helloworld/App.java
@@ -0,0 +1,19 @@
+package example.helloworld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	for(int i=0;i<4;i++){
+        System.out.println( "Hello Good World!" );
+    	}
+    }
+    
+    
+    
+    
+}
